98a75eb1b95cfc78ea9b0da677198ce3c63b8a4c
srikan1h <saisrikanth2007@gmail.com>
2026-04-21 10:39:34 +0530

    feature/uc2

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
-public class Main{
-#uc1
+import java.util.Random;
+
+public class TicTacToe {
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
 
-        // Initialize board with '-'
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+        String player1 = "Player 1";
+        String player2 = "Player 2";
+
+        char player1Symbol, player2Symbol;
+        String currentPlayer;
+        char currentSymbol;
+
+        // Random toss
+        Random rand = new Random();
+        int toss = rand.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+            currentPlayer = player1;
+            currentSymbol = player1Symbol;
+        } else {
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+            currentPlayer = player2;
+            currentSymbol = player2Symbol;
         }
 
-        // Display board
-        System.out.println("Tic-Tac-Toe Board:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(); 
-        }
+        // Display results
+        System.out.println("Toss Result:");
+        System.out.println(player1 + " is " + player1Symbol);
+        System.out.println(player2 + " is " + player2Symbol);
+        System.out.println(currentPlayer + " starts first with symbol " + currentSymbol);
     }
 }
